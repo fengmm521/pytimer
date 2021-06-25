5ab3f25621797873d4860226edb074208503013e
woodcol <mage@woodcol.com>
2021-06-25 22:25:55 +0800

    Update pytimer.py

--- pytimer.py
+++ pytimer.py
@@ -12,6 +12,7 @@ class _timerThread(threading.Thread):
         self.lasttimes = {}
         self.isOnceRuns = []
         self.threadFuncRun = {}
+        self.threadDatas = {}
         self.queue = queue
         self.timePrecision = timePrecision
     def setNewTimer(self,newobj):
@@ -22,25 +23,29 @@ class _timerThread(threading.Thread):
                 self.lasttimes = {}
                 self.isOnceRuns = []
                 self.threadFuncRun = {}
+                self.threadDatas = {}
             else:
                 self.threadtimes.remove(newobj.secendtime)
                 self.threadFunc[str(newobj.secendtime)] = None
                 self.lasttimes[str(newobj.secendtime)] = None
                 self.threadFuncRun[str(newobj.secendtime)] = None
+                self.threadDatas[str(newobj.secendtime)] = None
                 if newobj.secendtime in self.isOnceRuns:
                     self.isOnceRuns.remove(newobj.secendtime)
         else:
             if newobj.secendtime in self.threadtimes:
                 self.threadFunc[str(newobj.secendtime)] = newobj.func
-                self.lasttimes[str(newobj.secendtime)] = int(time.time())
+                self.lasttimes[str(newobj.secendtime)] = time.time()
                 self.threadFuncRun[str(newobj.secendtime)] = False
+                self.threadDatas[str(newobj.secendtime)] = newobj.datas
                 if newobj.isOnce:
                     self.isOnceRuns.append(newobj.secendtime)
             else:
                 self.threadtimes.append(newobj.secendtime)
                 self.threadFunc[str(newobj.secendtime)] = newobj.func
-                self.lasttimes[str(newobj.secendtime)] = int(time.time())
+                self.lasttimes[str(newobj.secendtime)] = time.time()
                 self.threadFuncRun[str(newobj.secendtime)] = False
+                self.threadDatas[str(newobj.secendtime)] = newobj.datas
                 if newobj.isOnce:
                     self.isOnceRuns.append(newobj.secendtime)
     def run(self):
@@ -49,11 +54,11 @@ class _timerThread(threading.Thread):
             if not self.queue.empty():
                 objtmp = self.queue.get()
                 self.setNewTimer(objtmp)
-            timetmp = int(time.time())
+            timetmp = time.time()
             for tx in self.threadtimes:
                 if timetmp - self.lasttimes[str(tx)] >= tx:
                     self.lasttimes[str(tx)] = timetmp
-                    self.threadFunc[str(tx)](timetmp)
+                    self.threadFunc[str(tx)](timetmp,self.threadDatas[str(tx)])
                     self.threadFuncRun[str(tx)] = True
             romveOnces = []
             for tone in self.isOnceRuns:
@@ -68,14 +73,15 @@ class _timerThread(threading.Thread):
         self.condition.release()
         
 class _timerObj():
-    def __init__(self,secendt,funct,isOnce = False):
+    def __init__(self,secendt,funct,datas = None,isOnce = False):
         self.secendtime = secendt
         self.func = funct
         self.isOnce = isOnce
+        self.datas = datas
 
 class pytimer():
     #timePrecision为时间精度，时间精度越高越占CPU时间，默认为100毫秒精度
-    def __init__(self,timePrecision = 0.1):
+    def __init__(self,timePrecision = 0.0001):
         self.queue = Queue()   
         self.cond = threading.Condition()
         self.t_thread = _timerThread(str(int(time.time())),self.queue, self.cond,timePrecision) 
@@ -86,8 +92,8 @@ class pytimer():
         self.t_thread.start()
     def getTimers(self):
         return self._timers
-    def setTimer(self,secendTime,Func,isOnce = False):
-        objtmp = _timerObj(secendTime,Func,isOnce)
+    def setTimer(self,secendTime,Func,datas,isOnce = False):
+        objtmp = _timerObj(secendTime,Func,datas,isOnce)
         self._timers.append(secendTime)
         self.queue.put(objtmp)
     def removeTimer(self,secendTime):
@@ -102,7 +108,7 @@ class pytimer():
 
 
 def main():  
-    def timerCallBack(timex):
+    def timerCallBack(timex,datas):
         print(timex)
     timerx = pytimer()
     timerx.setTimer(2, timerCallBack)
